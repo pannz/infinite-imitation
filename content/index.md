0e2c9d25ab7dbd481040a7578679f72773c314b5
Zhifan Pan <wojiaopanzhifan@gmail.com>
2025-04-02 14:15:40 +0800

    Quartz sync: Apr 2, 2025, 2:15 PM

---
+++ content/index.md
@@ -0,0 +1,9 @@
+---
+title: Hello, Inifinte Imitators
+---
+
+The world is an infinite games, we are all imitators! Initial imitation, later curation, finally creation. 
+
+Imitation is practice!
+
+![Free A person silhouetted against light at the end of a spiral tunnel, creating a striking black and white contrast. Stock Photo](https://images.pexels.com/photos/211816/pexels-photo-211816.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,15 +8,15 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Zhifan",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    locale: "zh-CN",
+    baseUrl: "panzf.com",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
